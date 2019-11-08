badbc9dce508463004c743efa3627493035e9259
Enrico Starke <enrico.starke@outlook.de>
2019-11-08 12:09:08 +0100

    Added Login

--- NameRegistryDatabase/Security/nameregistryuser.sql
+++ NameRegistryDatabase/Security/nameregistryuser.sql
@@ -1,2 +1,5 @@
-﻿CREATE USER [nameregistryuser] FOR LOGIN [nameregistryuser];
+﻿CREATE LOGIN [nameregistryuser] WITH PASSWORD = 'fgnksjg56956805gg!';
+GO
+
+CREATE USER [nameregistryuser] FOR LOGIN [nameregistryuser];
 
--- NameRegistryDatabase/dbo/Tables/NamePattern.sql
+++ NameRegistryDatabase/dbo/Tables/NamePattern.sql
@@ -1,5 +1,7 @@
 ﻿CREATE TABLE [dbo].[NamePattern]
 (
 	[ID] INT NOT NULL PRIMARY KEY, 
+    [ServiceID] INT NULL, 
+    [NamePattern] NVARCHAR(4000) NULL, 
     CONSTRAINT [FK_NamePattern_Service] FOREIGN KEY ([ServiceID]) REFERENCES [Service]([ID])
 )
